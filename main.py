1effb6407cc62b799eaea053d81d7667cd999750
Bryan <bhowiebkr@gmail.com>
2020-11-23 22:45:42 -0500

    added a node list widget

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import os
 
 from node_editor.gui.node_widget import NodeWidget
 from node_editor.gui.palette import palette
+from node_editor.gui.node_list import NodeList
 
 logging.basicConfig(level=logging.DEBUG)
 
@@ -20,27 +21,38 @@ class NodeEditor(QtWidgets.QMainWindow):
         icon = QtGui.QIcon("resources\\app.ico")
         self.setWindowIcon(icon)
 
-        self.setWindowTitle("Node Editor")
-        settings = QtCore.QSettings("Howard", "NodeEditor")
-
-        try:
-            self.restoreGeometry(settings.value("geometry"))
-        except AttributeError:
-            logging.warning("Unable to load settings. First time opening the tool?")
+        self.setWindowTitle("Logic Node Editor")
+        settings = QtCore.QSettings("node-editor", "NodeEditor")
 
         # Layouts
         main_widget = QtWidgets.QWidget()
-        main_layout = QtWidgets.QVBoxLayout()
+        main_layout = QtWidgets.QHBoxLayout()
+
+        self.node_list = NodeList()
+        self.splitter = QtWidgets.QSplitter()
 
         self.setCentralWidget(main_widget)
         main_widget.setLayout(main_layout)
 
         self.node_widget = NodeWidget(self)
-        main_layout.addWidget(self.node_widget)
+        main_layout.addWidget(self.splitter)
+        self.splitter.addWidget(self.node_list)
+        self.splitter.addWidget(self.node_widget)
+
+        try:
+            self.restoreGeometry(settings.value("geometry"))
+            s = settings.value("splitterSize")
+            self.splitter.restoreState(s)
+
+        except AttributeError as e:
+            logging.warning(
+                "Unable to load settings. First time opening the tool?\n" + str(e)
+            )
 
     def closeEvent(self, event):
         self.settings = QtCore.QSettings("node-editor", "NodeEditor")
         self.settings.setValue("geometry", self.saveGeometry())
+        self.settings.setValue("splitterSize", self.splitter.saveState())
         QtWidgets.QWidget.closeEvent(self, event)
 
 
