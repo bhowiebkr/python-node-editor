90bd928ba0c6c55ba3da5824a7f90fa86f0e4192
Bryan <bhowiebkr@gmail.com>
2020-11-23 22:47:11 -0500

    creation of custom nodes

--- node_editor/gui/node_widget.py
+++ node_editor/gui/node_widget.py
@@ -1,38 +1,104 @@
-from PySide2.QtWidgets import QWidget, QVBoxLayout, QGraphicsScene
+# from PySide2.QtWidgets import QWidget, QVBoxLayout, QGraphicsScene
+
+from PySide2 import QtWidgets, QtGui
 
 from node_editor.gui.view import View
 from node_editor.gui.node import Node
 from node_editor.gui.node_editor import NodeEditor
 
-import lorem
-import random
+# import lorem
+# import random
 
 
-class NodeWidget(QWidget):
+def create_input():
+    node = Node()
+    node.title = "A"
+    node.type_text = "input"
+    node.add_port(name="output", is_output=True)
+    node.build()
+    return node
+
+
+def create_output():
+    node = Node()
+    node.title = "A"
+    node.type_text = "output"
+    node.add_port(name="input", is_output=False)
+    node.build()
+    return node
+
+
+def create_and():
+    node = Node()
+    node.title = "AND"
+    node.type_text = "built-in"
+    node.add_port(name="input A", is_output=False)
+    node.add_port(name="input B", is_output=False)
+    node.add_port(name="output", is_output=True)
+    node.build()
+    return node
+
+
+def create_not():
+    node = Node()
+    node.title = "NOT"
+    node.type_text = "built-in"
+    node.add_port(name="input", is_output=False)
+    node.add_port(name="output", is_output=True)
+    node.build()
+    return node
+
+
+class NodeScene(QtWidgets.QGraphicsScene):
+    def dragEnterEvent(self, e):
+        e.acceptProposedAction()
+
+    def dropEvent(self, e):
+        # find item at these coordinates
+        item = self.itemAt(e.scenePos())
+        if item.setAcceptDrops == True:
+            # pass on event to item at the coordinates
+            try:
+                item.dropEvent(e)
+            except RuntimeError:
+                pass  # This will supress a Runtime Error generated when dropping into a widget with no ProxyWidget
+
+    def dragMoveEvent(self, e):
+        e.acceptProposedAction()
+
+
+class NodeWidget(QtWidgets.QWidget):
     def __init__(self, parent):
         super(NodeWidget, self).__init__(parent)
-        main_layout = QVBoxLayout()
+        main_layout = QtWidgets.QVBoxLayout()
         main_layout.setContentsMargins(0, 0, 0, 0)
         self.setLayout(main_layout)
 
         self.node_editor = NodeEditor(self)
-        self.scene = QGraphicsScene()
+        self.scene = NodeScene()
+        self.scene.setSceneRect(0, 0, 9999, 9999)
         self.view = View(self)
         self.view.setScene(self.scene)
         self.node_editor.install(self.scene)
 
         main_layout.addWidget(self.view)
 
-        self.create_random_nodes(10)
+        self.view.request_node.connect(self.create_node)
 
-    def create_random_nodes(self, num):
+    def create_node(self, name):
+        print("creating node:", name)
 
-        for i in range(num):
-            node = Node()
-            for i in range(random.randrange(2, 10)):
-                word = lorem.sentence().split(" ")[0]
-                node.add_port(name=word, is_output=bool(random.getrandbits(1)))
-                node.title = "Title"
-                node.type = "example"
-            node.build()
-            self.scene.addItem(node)
+        if name == "Input":
+            node = create_input()
+
+        elif name == "Output":
+            node = create_output()
+        elif name == "And":
+            node = create_and()
+        elif name == "Not":
+            node = create_not()
+
+        self.scene.addItem(node)
+
+        pos = self.view.mapFromGlobal(QtGui.QCursor.pos())
+        node.setPos(self.view.mapToScene(pos))
