91ad07d61a31615a819c225b4e5e47f1a86ca8fa
Bryan <bhowiebkr@gmail.com>
2023-03-04 16:15:46 -0500

    restructuring left side, added a button for making new node types

--- main.py
+++ main.py
@@ -6,10 +6,16 @@ import logging
 import os
 
 from node_editor.gui.node_widget import NodeWidget
-from node_editor.gui.palette import palette
 from node_editor.gui.node_list import NodeList
+from node_editor.gui.node_type_editor import NodeTypeEditor
 
 logging.basicConfig(level=logging.DEBUG)
+
+import importlib
+
+# Import qdarktheme if you have it. If not install it with pip. Dark Themese are great!
+if importlib.util.find_spec("qdarktheme") is not None:
+    import qdarktheme
 
 
 class NodeEditor(QtWidgets.QMainWindow):
@@ -20,29 +26,50 @@ class NodeEditor(QtWidgets.QMainWindow):
         icon = QtGui.QIcon("resources\\app.ico")
         self.setWindowIcon(icon)
 
-        self.setWindowTitle("Logic Node Editor")
+        self.setWindowTitle("Simple Node Editor")
         settings = QtCore.QSettings("node-editor", "NodeEditor")
 
         # Layouts
         main_widget = QtWidgets.QWidget()
-        main_layout = QtWidgets.QHBoxLayout()
-
-        self.node_list = NodeList()
-        self.splitter = QtWidgets.QSplitter()
-
         self.setCentralWidget(main_widget)
+        main_layout = QtWidgets.QHBoxLayout()
         main_widget.setLayout(main_layout)
+        left_layout = QtWidgets.QVBoxLayout()
+        left_layout.setContentsMargins(0, 0, 0, 0)
 
+        # Widgets
+        self.node_list = NodeList()
+        left_widget = QtWidgets.QWidget()
+        self.splitter = QtWidgets.QSplitter()
         self.node_widget = NodeWidget(self)
-        main_layout.addWidget(self.splitter)
-        self.splitter.addWidget(self.node_list)
-        self.splitter.addWidget(self.node_widget)
+        new_node_type_btn = QtWidgets.QPushButton("New Node Type")
+        new_node_type_btn.setFixedHeight(50)
 
+        # Add Widgets to layouts
+        self.splitter.addWidget(left_widget)
+        self.splitter.addWidget(self.node_widget)
+        left_widget.setLayout(left_layout)
+        left_layout.addWidget(self.node_list)
+        left_layout.addWidget(new_node_type_btn)
+        main_layout.addWidget(self.splitter)
+
+        # Logic
+        new_node_type_btn.clicked.connect(self.new_node_cmd)
+
+        # Restore GUI from last state
         if settings.contains("geometry"):
             self.restoreGeometry(settings.value("geometry"))
 
             s = settings.value("splitterSize")
             self.splitter.restoreState(s)
+
+    def new_node_cmd(self):
+        node_editor = NodeTypeEditor()
+
+        if node_editor.exec() == QtWidgets.QDialog.Accepted:
+            print("Dialog accepted")
+        else:
+            print("Dialog canceled")
 
     def closeEvent(self, event):
         self.settings = QtCore.QSettings("node-editor", "NodeEditor")
@@ -54,7 +81,8 @@ class NodeEditor(QtWidgets.QMainWindow):
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
     app.setWindowIcon(QtGui.QIcon("resources\\app.ico"))
-    app.setPalette(palette)
+    qdarktheme.setup_theme()
+
     launcher = NodeEditor()
     launcher.show()
     app.exec()
