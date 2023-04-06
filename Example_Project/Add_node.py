7fc5bc11a00bae14da4e2df0b89ccf7964e5d35f
Bryan <bhowiebkr@gmail.com>
2023-04-06 01:50:34 -0400

    adding execution pins

--- Example_Project/Add_node.py
+++ Example_Project/Add_node.py
@@ -7,6 +7,11 @@ class Add_Node(Node):
 
         self.title = "Add"
         self.type_text = "Logic Nodes"
+
+        self.add_port(name="Ex In", is_output=False, execution=True)
+        self.add_port(name="Ex Out", is_output=True, execution=True)
+
+        self.add_port(name="input A", is_output=False)
         self.add_port(name="input A", is_output=False)
         self.add_port(name="input B", is_output=False)
         self.add_port(name="output", is_output=True)
