cf9103da67cc48c75cde74dc824a800f1d146ec0
Bryan <bhowiebkr@gmail.com>
2023-04-08 00:48:57 -0400

    a scaler node

---
+++ Example_Project/Scaler_node.py
@@ -0,0 +1,30 @@
+from PySide6 import QtWidgets
+
+from node_editor.gui.node import Node
+from Example_Project.common_widgets import FloatLineEdit
+
+
+class Scaler_Node(Node):
+    def __init__(self):
+        super().__init__()
+
+        self.title = "Scaler"
+        self.type_text = "Constants"
+        self.set_color(title_color=(255, 165, 0))
+
+        self.add_port(name="value", is_output=True)
+
+        self.build()
+
+    def init_widget(self):
+        self.widget = QtWidgets.QWidget()
+        layout = QtWidgets.QVBoxLayout()
+        self.scaler_line = FloatLineEdit()
+        layout.addWidget(self.scaler_line)
+        self.widget.setLayout(layout)
+
+        proxy = QtWidgets.QGraphicsProxyWidget()
+        proxy.setWidget(self.widget)
+        proxy.setParentItem(self)
+
+        super().init_widget()
